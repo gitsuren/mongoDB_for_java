76e787ebd1e809b48054c61093e777312065097a
unknown <bajracharya.surendra@gmail.com>
2013-03-17 12:44:56 -0500

    Commit all once again

--- src/main/java/course/UserDAO.java
+++ src/main/java/course/UserDAO.java
@@ -43,24 +43,17 @@ public class UserDAO {
 
         String passwordHash = makePasswordHash(password, Integer.toString(random.nextInt()));
 
-        // XXX WORK HERE
-        // create an object suitable for insertion into the user collection
-        // be sure to add username and hashed password to the document. problem instructions
-        // will tell you the schema that the documents must follow.
+        BasicDBObject user = new BasicDBObject();
 
+        user.append("_id", username).append("password", passwordHash);
 
-        BasicDBObject object = new BasicDBObject("_id",username);
-        object.append("password",passwordHash);
         if (email != null && !email.equals("")) {
-            // XXX WORK HERE
-            // if there is an email address specified, add it to the document too.
-            object.append("email",email);
+            // the provided email address
+            user.append("email", email);
         }
 
         try {
-            // XXX WORK HERE
-            // insert the document into the user collection here
-            usersCollection.insert(object);
+            usersCollection.insert(user);
             return true;
         } catch (MongoException.DuplicateKey e) {
             System.out.println("Username already in use: " + username);
@@ -69,11 +62,9 @@ public class UserDAO {
     }
 
     public DBObject validateLogin(String username, String password) {
-        DBObject user = null;
+        DBObject user;
 
-        // XXX look in the user collection for a user that has this username
-        // assign the result to the user variable.
-        user=usersCollection.findOne(new BasicDBObject("_id",username));
+        user = usersCollection.findOne(new BasicDBObject("_id", username));
 
         if (user == null) {
             System.out.println("User not in database");
